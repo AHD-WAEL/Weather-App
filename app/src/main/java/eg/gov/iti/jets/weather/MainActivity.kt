8984eadd05757e9a2bea6b70c52e4a5c50a318a3
ahd <ahdwael28@gmail.com>
2023-03-19 17:37:37 +0200

    GUI Design

---
+++ app/src/main/java/eg/gov/iti/jets/weather/MainActivity.kt
@@ -0,0 +1,41 @@
+package eg.gov.iti.jets.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import eg.gov.iti.jets.weather.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.fabBtn.setOnClickListener {
+            replaceFragment(HomeFragment())
+        }
+
+        binding.bottomNav.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.favourite -> replaceFragment(FavouriteFragment())
+                R.id.alert -> replaceFragment(AlertFragment())
+                R.id.setting -> replaceFragment(SettingFragment())
+
+                else ->{}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.containerFrameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
